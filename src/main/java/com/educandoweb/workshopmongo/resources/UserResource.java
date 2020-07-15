4c1f5a0f9ed403c3fd3509f01d5443bcd649f044
rsenoi <ricardo.senoi@yahoo.com.br>
2020-07-14 21:56:12 -0300

    Delecao de usuario

--- src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
@@ -56,4 +56,12 @@ public class UserResource<T> {
 		
 	}
 
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+		
+	}
+
 }
