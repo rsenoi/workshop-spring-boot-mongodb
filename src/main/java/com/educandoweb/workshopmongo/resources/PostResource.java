2a91906daee21e805ebb0849af5b9912c79aca01
rsenoi <ricardo.senoi@yahoo.com.br>
2020-07-16 22:16:51 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/educandoweb/workshopmongo/resources/PostResource.java
+++ src/main/java/com/educandoweb/workshopmongo/resources/PostResource.java
@@ -4,24 +4,15 @@ package com.educandoweb.workshopmongo.resources;
 
 
 
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.workshopmongo.domain.Post;
-import com.educandoweb.workshopmongo.domain.User;
-import com.educandoweb.workshopmongo.dto.UserDTO;
 import com.educandoweb.workshopmongo.services.PostService;
-import com.educandoweb.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value="/posts")
