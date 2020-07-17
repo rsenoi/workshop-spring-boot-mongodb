a056e5bdfe79d74e47ec1af8904bf89be4ff481a
rsenoi <ricardo.senoi@yahoo.com.br>
2020-07-16 21:55:36 -0300

    Obtendo Post por ID

---
+++ src/main/java/com/educandoweb/workshopmongo/resources/PostResource.java
@@ -0,0 +1,44 @@
+package com.educandoweb.workshopmongo.resources;
+
+
+
+
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.educandoweb.workshopmongo.domain.Post;
+import com.educandoweb.workshopmongo.domain.User;
+import com.educandoweb.workshopmongo.dto.UserDTO;
+import com.educandoweb.workshopmongo.services.PostService;
+import com.educandoweb.workshopmongo.services.UserService;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource<T> {
+
+	@Autowired
+	private PostService service;
+	
+
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id){
+		Post obj = service.findById(id);
+		
+		return ResponseEntity.ok().body( obj );
+		
+	}
+	
+
+
+}
